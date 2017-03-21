320cc8e9003fa3f65c6410b030092e1c79fbee2e
jwaltzjr <jwaltzjr@hotmail.com>
2017-03-21 14:44:47 -0500

    Started ontimereport

---
+++ truckmate/ontimereport.py
@@ -0,0 +1,47 @@
+import os
+import smtplib
+import sys
+
+import openpyxl
+import pandas
+
+from email.MIMEMultipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+import database
+
+REPORT_EMAILS = ['jwaltzjr@krclogistics.com']
+
+def email_spreadsheet(email_addresses, spreadsheet):
+    email_username = 'reports@krclogistics.com'
+    email_password = 'General1'
+    
+    # Create email
+    email_message = MIMEMultipart('alternative')
+
+    email_message['To'] = ', '.join(email_addresses)
+    email_message['From'] = email_username
+    email_message['Subject'] = 'Weekly Tonnage'
+
+    # Attach Spreadsheet
+    attachment = MIMEApplication(spreadsheet)
+    attachment['Content-Disposition'] = 'attachment; filename="%s"' % 'weekly_tonnage.xlsx'
+    email_message.attach(attachment)
+
+    # Connect to server and send email
+    server = smtplib.SMTP('smtp.office365.com', 587)
+    server.starttls()
+    server.login(email_username, email_password)
+    server.sendmail(email_username, email_addresses, email_message.as_string())
+    server.quit()
+
+def main():
+    sql_file_path = os.path.join(sys.path[0], 'ontimereport.sql')
+    with open(sql_file_path, 'r') as sql_file:
+        sql_query = sql_file.read()
+
+    dataset = pandas.read_sql(sql_query, database.truckmate.connection)
+    print dataset
+
+if __name__ == '__main__':
+    main()
