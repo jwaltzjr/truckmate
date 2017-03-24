eb17c733ef19604d0aa924709981c80b41054261
jwaltzjr <jwaltzjr@hotmail.com>
2017-03-24 10:28:26 -0500

    Add export to csv for debugging

--- truckmate/ontimereport.py
+++ truckmate/ontimereport.py
@@ -110,4 +110,6 @@ print dataset['ONTIME_APPT'].mean()
 print dataset['ONTIME_APPT_REALISTIC'].mean()
 print dataset['ONTIME_DELV'].mean()
 
+dataset.to_csv('ontimereport.csv')
+
 print dataset.groupby(['DELIVERY_WEEK','DELIVERY_TERMINAL'])[['ONTIME_APPT','ONTIME_APPT_REALISTIC','ONTIME_DELV']].mean()
