78d3fe6540aa5b280e3f304ce0d702d97c5e687d
jwaltzjr <jwaltzjr@hotmail.com>
2017-04-21 11:22:58 -0500

    Finished the if/else block I only did halfway

--- truckmate/ratereport.py
+++ truckmate/ratereport.py
@@ -59,6 +59,7 @@ class RateReport(object):
             if 600 <= rate.DESTINATION[:3] <= 606:
                 return 'CHICOMM'
             else:
+                return rate.DESTINATION[:3]
         else:
             return rate.DESTINATION
 
