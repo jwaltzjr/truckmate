93ac01aa82c4468719825ec4dbde364884dcbe6c
Jeff Waltz <jwaltzjr@Staley.local>
2020-09-01 13:20:36 -0500

    Update db and and debug func

--- truckmate/database.py
+++ truckmate/database.py
@@ -4,8 +4,20 @@ from krc.database import DatabaseConnection
 
 truckmate = DatabaseConnection(
     '{IBM DB2 ODBC DRIVER - DB2COPY1}',
-    'TM_Reporting_00001',
+    'TM_Reporting_SVC_00001',
+    'STAY-DB201:50000',
     'STALEY',
     env.db_user.value,
     env.db_password.value
 )
+
+if __name__ == '__main__':
+    with truckmate as db:
+        print('Connected successfully')
+        with db.connection.cursor() as cursor:
+            print('Cursor opened')
+            cursor.execute('SELECT * FROM TLORDER FETCH FIRST ROW ONLY')
+            print('Cursor result:\n%s' % cursor.fetchall())
+        print('Cursor commited or rolled back')
+    print('Connection closed successfully')
+            
